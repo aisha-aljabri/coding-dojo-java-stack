b385f1139bbbd74acaec4f1cda8316466245262f
aisha-aljabri <aisha,.aljabri15@gmail.com>
2021-01-04 15:16:39 +0300

    initial commit

--- JAVA_Fundamentals/String Manipulation/Manipulatormain.java
+++ JAVA_Fundamentals/String Manipulation/Manipulatormain.java
@@ -1,7 +1,30 @@
 public class Manipulatormain{
     public static void main(String args[]) {
+
         StringManipulator test = new StringManipulator();
-        String result = test.trimAndConcat("aisha  ", "   aljabri  ");
-        System.out.print(result);
+        String result = test.trimAndConcat("  Hello  ", "   World  ");
+        System.out.println(result);
+
+        char letter = 'o';
+        Integer a = test.getIndexOrNull("Coding", letter);
+        Integer b = test.getIndexOrNull("Hello World", letter);
+        Integer c = test.getIndexOrNull("Hi", letter);
+        System.out.println(a); // 1
+        System.out.println(b); // 4
+        System.out.println(c);
+
+        String word = "Hello";
+        String subString = "llo";
+        String notSubString = "world";
+        Integer a1 = test.getIndexOrNull(word, subString);
+        Integer b1 = test.getIndexOrNull(word, notSubString);
+        System.out.println(a1); // 2
+        System.out.println(b1);
+
+
+        String word1 = test.concatSubstring("Hello", 1, 2, "world");
+        System.out.println(word1); // eworld
     }
+
+
 }
--- JAVA_Fundamentals/String Manipulation/StringManipulator.java
+++ JAVA_Fundamentals/String Manipulation/StringManipulator.java
@@ -2,4 +2,26 @@ public class StringManipulator{
     public String trimAndConcat(String x, String y){
         return x.trim().concat(y.trim());
     }
+
+    public Integer getIndexOrNull(String str, char ch){
+        if (str.indexOf(ch) >= 0){
+            return str.indexOf(ch);
+        }
+        else {
+            return null;
+        }
+    }
+
+    public Integer getIndexOrNull(String str1, String str2){
+        if (str1.indexOf(str2) >= 0){
+            return str1.indexOf(str2);
+        }
+        else{
+            return null;
+        }
+    }
+
+    String concatSubstring(String str1, int i1, int i2, String str2){
+        return str1.substring(i1, i2).concat(str2);
+    }
 }
