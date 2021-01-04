591347a064a59ef17a4f9e48986c16bb0ce0e63d
aisha-aljabri <aisha,.aljabri15@gmail.com>
2021-01-04 14:40:05 +0300

    initial commit

---
+++ JAVA_Fundamentals/String Manipulation/Manipulatormain.java
@@ -0,0 +1,7 @@
+public class Manipulatormain{
+    public static void main(String args[]) {
+        StringManipulator test = new StringManipulator();
+        String result = test.trimAndConcat("aisha  ", "   aljabri  ");
+        System.out.print(result);
+    }
+}
---
+++ JAVA_Fundamentals/String Manipulation/StringManipulator.java
@@ -0,0 +1,5 @@
+public class StringManipulator{
+    public String trimAndConcat(String x, String y){
+        return x.trim().concat(y.trim());
+    }
+}
