6a741866bc20433daea952e694d21833aa13cc69
aisha-aljabri <aisha,.aljabri15@gmail.com>
2021-01-05 14:57:56 +0300

    initial commit

---
+++ JAVA oop/Singly Linked List/SinglyLinkedList.java
@@ -0,0 +1,51 @@
+public class SinglyLinkedList {
+    public Node head;
+    public SinglyLinkedList() {
+        this.head=null;
+    }
+    // SLL methods go here. As a starter, we will show you how to add a node to the list.
+    public void add(int value) {
+        Node newNode = new Node(value);
+        if(head == null) {
+            head = newNode;
+        } else {
+            Node runner = head;
+            while(runner.next != null) {
+                runner = runner.next;
+            }
+            runner.next = newNode;
+        }
+    }    
+
+    public void remove(){
+        if(head != null){
+            if(head.next == null){
+                head=null;
+            }
+            else{
+                Node runner = head;
+                while(runner.next != null){
+                    if(runner.next.next == null){
+                        runner.next=null;
+                    }
+                    else{
+                        runner=runner.next;
+                    }
+                }
+            }
+        }
+    }
+
+    public void printValues(){
+        if(head == null){
+            System.out.println("list is empty!");
+        }
+        else{
+            Node runner = head;
+            while(runner != null){
+                System.out.println(runner.value);
+                runner=runner.next;
+            }
+        }
+    }
+}
