2ca4b387a00791a7b083416e6c25ff5f10556d5a
aisha-aljabri <aisha,.aljabri15@gmail.com>
2021-01-15 08:06:30 +0300

    initial commet

---
+++ Java Spring/mvc/src/main/java/com/codingdojo/mvc/services/BookService.java
@@ -0,0 +1,51 @@
+package com.codingdojo.mvc.services;
+import java.util.Date;
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.stereotype.Service;
+
+import com.codingdojo.mvc.models.Book;
+import com.codingdojo.mvc.repositories.BookRepository;
+@Service
+public class BookService {
+private final BookRepository bookRepository;
+    
+    public BookService(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+    // returns all the books
+    public List<Book> allBooks() {
+        return bookRepository.findAll();
+    }
+    // creates a book
+    public Book createBook(Book b) {
+        return bookRepository.save(b);
+    }
+    // retrieves a book
+    public Book findBook(Long id) {
+        Optional<Book> optionalBook = bookRepository.findById(id);
+        if(optionalBook.isPresent()) {
+            return optionalBook.get();
+        } else {
+            return null;
+        }
+    }
+    public Book updateBook(Long id, String title, String desc, String lang, Integer numOfPages) {
+    	Optional <Book> book = bookRepository.findById(id);
+    	if(book != null) {
+    		Book theBook = book.get();
+    		theBook.setTitle(title);
+    		theBook.setDescription(desc);
+    		theBook.setLanguage(lang);
+    		theBook.setNumberOfPages(numOfPages);
+    		return bookRepository.save(theBook);
+    	}
+    	return null;        
+    }
+    
+    // deletes a book
+    public void deleteBook(Long id) {
+    	bookRepository.deleteById(id);
+    }
+}
